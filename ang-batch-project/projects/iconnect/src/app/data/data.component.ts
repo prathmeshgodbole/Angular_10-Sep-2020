d5f28099d8a5df1ace6e9f20afb603528951ad93
prathmeshgodbole <godbole611@gmail.com>
2020-10-13 16:32:26 +0530

    directives

--- ang-batch-project/projects/iconnect/src/app/data/data.component.ts
+++ ang-batch-project/projects/iconnect/src/app/data/data.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { v4 as uuidv4 } from 'uuid';
+import { TabData } from './domain';
 
 @Component({
   selector: 'app-data',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataComponent implements OnInit {
 
+  details: Array<TabData> = new Array()
+
+
   constructor() { }
 
   ngOnInit(): void {
+    this.details.push({ id: uuidv4(),  Company_Name: 'Pen', Company_Description: 'a', Contact_Number: 111111, Email: 'abc@gmail.com', State: 'Goa' })
+    this.details.push({ id: uuidv4(),  Company_Name: 'Pencil', Company_Description: 'b', Contact_Number: 22222, Email: 'pqr@gmail.com', State: 'Goa' })
+    this.details.push({ id: uuidv4(),  Company_Name: 'Eraser', Company_Description: 'c', Contact_Number: 33333, Email:'abc@gmail.com', State: 'Goa' })
+    this.details.push({ id: uuidv4(),  Company_Name: 'Sharpner', Company_Description: 'd', Contact_Number:44444, Email: 'xyz@gmail.com', State: 'Goa' })
+    this.details.push({ id: uuidv4(),  Company_Name: 'NoteBook', Company_Description: 'e', Contact_Number: 55555, Email: 'bob@gmail.com', State: 'Goa' })
+
+
+    console.log(this.details)
+  }
+
+  addData( Company_Name: string, Company_Description: string, Contact_Number: number, Email: string,State:string) {
+    this.details.push({ id: uuidv4(),  Company_Name, Company_Description, Contact_Number, Email, State })
   }
 
 }
---
+++ ang-batch-project/projects/iconnect/src/app/data/domain.ts
@@ -0,0 +1,9 @@
+export interface TabData {
+  id:string,
+  Company_Name: string
+  Company_Description: string
+  Contact_Number: number
+  Email: string
+  State: string
+
+}
